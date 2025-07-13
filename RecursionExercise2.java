623a849480484afb6663e6b68abb7633b3daf8e9
Alvin Mugoya <mugoyaalvin018@gmail.com>
2025-07-13 22:54:56 +0300

    RecursionExercise2.java

---
+++ RecursionExercise2.java
@@ -0,0 +1,11 @@
+public class RecursionExercise2 {
+    public static int factorial(int n){
+        if(n==0||n==1){
+            return 1;
+        }
+        return n *factorial(n-1);
+    }
+    public static void main(String[] args){
+        System.out.println("The factorial of 5 is : "+factorial(5));
+    }
+}
