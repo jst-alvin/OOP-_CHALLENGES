4b9dd64f849bf505d8029a2de9b814cc31c8de7c
Alvin Mugoya <mugoyaalvin018@gmail.com>
2025-07-13 22:48:51 +0300

    JavaBasicsChallenge.java

---
+++ JavaBasicsChallenge.java
@@ -0,0 +1,9 @@
+public class JavaBasicsChallenge {
+    public static void greet(){
+        System.out.println("Hello there");
+
+    }
+    public static void main(String[] args){
+        JavaBasicsChallenge.greet();
+    }
+}
